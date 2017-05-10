5c0ef0cd5a2beee858d71da3994e5f1f8fd1f532
Qhenckel <boardman94@gmail.com>
2017-05-10 13:10:27 -0500

    Added scrap spawning

---
+++ Assets/Scripts/ScrapSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrapSpawner : MonoBehaviour {
+
+	public float SpawnRadius = 5;
+	public float Spawnfreq = 10;
+	public float SpawnChance = 0.5f;
+	public GameObject[] SpawnObjects;
+	
+	private Transform spawnCenter;
+
+	// Use this for initialization
+	void Start () {
+		spawnCenter = this.transform;
+
+		InvokeRepeating ("Spawn", Spawnfreq, Spawnfreq);
+	}
+	
+	void Spawn () {
+		if(Random.value <= SpawnChance) {
+			Vector3 rand = Random.insideUnitCircle * SpawnRadius;
+			rand.z = rand.y;
+			rand.y = 0;
+			Vector3 pos = spawnCenter.position + rand;
+			int i = Random.Range(0, SpawnObjects.Length);
+			Quaternion rot = Random.rotation;
+			Instantiate(SpawnObjects[i],pos, rot);
+		}
+	}
+}
