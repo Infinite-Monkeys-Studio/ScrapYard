2b013fa8d0a25946d094156315a6658d76f660d5
Qhenckel <boardman94@gmail.com>
2017-05-10 13:47:35 -0500

    Added better machine mechanics

--- Assets/Scripts/deleteScrap.cs
+++ Assets/Scripts/deleteScrap.cs
@@ -2,12 +2,26 @@
 using System.Collections;
 
 public class deleteScrap : MonoBehaviour {
-
-	//public Collider ProcessArea;
-
+	
+	public string[] ValidInputs;
 
 	void OnTriggerEnter(Collider other) {
-		Destroy(other.gameObject);
+		Debug.Log ("Testing...");
+		if (isValidInput (other.gameObject)) {
+			Debug.Log ("Valid. destroy");
+			Destroy (other.gameObject);
+		} else {
+			Debug.Log("invalid");
+		}
+	}
+
+	bool isValidInput(GameObject obj) {
+		foreach(string inp in ValidInputs) {
+			if (obj.name == string.Format("{0}(Clone)", inp)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
